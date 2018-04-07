5acd2757086888e0d09866a3fbec92af4b114954
Carter Capocaccia <carter.capocaccia@gmail.com>
2018-04-06 23:23:11 -0500

    fixes issue with getting current leader, adds exit button for edit mode, refactors when to calculate top leader

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,10 +21,11 @@
         <button @click="logWin = false">Close</button>
       </div>
     </div>
-    <!--<h1 class="top-leader">Current leader is: {{ leader }} with {{ leaderCount }} wins!</h1>-->
-    <!--<h2 class="total-games">Total Games Played: {{ totalGames }}</h2>-->
+    <h1 class="top-leader">Current leader is: {{ leader }} with {{ leaderCount }} wins!</h1>
+    <h2 class="total-games">Total Games Played: {{ totalGames }}</h2>
     <h2 class="total-games">Current Game: Uno</h2>
     <div class="leaderboard">
+      <button v-if="editActive" @click="editActive = false"> Exit Edit Mode </button>
       <h2>Leaderboard</h2>
       <div class="winner" v-for="winner in winners">
         <div class="divider"></div>
@@ -33,32 +34,32 @@
             <span class="label">Winner:</span> {{ winner[0] }}
             <div v-if="editActive">
               <input type="text" v-model="updateText">
-              <button @click="updateWinner(winner.date, winner.key, 0)">Update</button>
-              <button @click="removeWinner(winner.date, winner.key, 0)">Delete</button>
+              <button @click="updateWinner(winner.parentKey, winner.key, 0)">Update</button>
+              <button @click="removeWinner(winner.parentKey, winner.key, 0)">Delete</button>
             </div>
           </div>
           <div class="winner-name" v-if="winner[1]">
             <span class="label">Second Place:</span> {{ winner[1] }}
             <div v-if="editActive">
               <input type="text" v-model="updateText">
-              <button @click="updateWinner(winner.date, winner.key, 1)">Update</button>
-              <button @click="removeWinner(winner.date, winner.key, 1)">Delete</button>
+              <button @click="updateWinner(winner.parentKey, winner.key, 1)">Update</button>
+              <button @click="removeWinner(winner.parentKey, winner.key, 1)">Delete</button>
             </div>
           </div>
           <div class="winner-name" v-if="winner[2]">
             <span class="label">Third Place:</span> {{ winner[2] }}
             <div v-if="editActive">
               <input type="text" v-model="updateText">
-              <button @click="updateWinner(winner.date, winner.key, 2)">Update</button>
-              <button @click="removeWinner(winner.date, winner.key, 2)">Delete</button>
+              <button @click="updateWinner(winner.parentKey, winner.key, 2)">Update</button>
+              <button @click="removeWinner(winner.parentKey, winner.key, 2)">Delete</button>
             </div>
           </div>
           <div class="winner-name" v-if="winner[3]">
             <span class="label">Fourth Place:</span> {{ winner[3] }}
             <div v-if="editActive">
               <input type="text" v-model="updateText">
-              <button @click="updateWinner(winner.date, winner.key, 3)"> Update</button>
-              <button @click="removeWinner(winner.date, winner.key, 3)">Delete</button>
+              <button @click="updateWinner(winner.parentKey, winner.key, 3)"> Update</button>
+              <button @click="removeWinner(winner.parentKey, winner.key, 3)">Delete</button>
             </div>
           </div>
           <div class="winner-date">
@@ -132,9 +133,8 @@ export default {
     logWinner () {
       this.logWin = true
     },
-    removeWinner (date, key, idx) {
-      let winnersKey = date.split(' ').join('')
-      db.ref(`/${winnersKey}/games/${key}/${idx}`).remove()
+    removeWinner (parentKey, key, idx) {
+      db.ref(`/${parentKey}/games/${key}/${idx}`).remove()
     },
     updateWinner (date, key, idx) {
       let winnersKey = date.split(' ').join('')
@@ -150,11 +150,15 @@ export default {
       let winners = [];
 
       for(var i = 0; i < this.winners.length; i++) {
-        //builds an obj of all winners and the number of games they have won
-        obj[this.winners[i].name] === undefined ? obj[this.winners[i].name] = 1 : obj[this.winners[i].name]++
+        let gameWinners = this.winners[i]; 
 
-        //updates total number of games played
+        //builds an obj of all winners and the number of games they have won
+        obj[gameWinners[0]] === undefined ? obj[gameWinners[0]] = 1 : obj[gameWinners[0]]++
+
+
+        // //updates total number of games played
         this.totalGames++
+
       }
 
       for(var key in obj) {
@@ -168,9 +172,10 @@ export default {
         }
 
         this.leader = winners.length > 1 ? 'Its a Tie Between ' + winners.join(',') : winners[0]
-      }
+      } 
+
     },
-    activateDelete () {
+    activateEdit () {
       const code = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter';
       let pressed = [];
       window.addEventListener('keyup', (e) => {
@@ -186,12 +191,14 @@ export default {
     allWinners.on('value', (snapshot) => {
       var winnersBank = [];
 
+
       //builds an array of all the winners in the DB
       snapshot.forEach((childSnap) => {
         let item = childSnap.val();
         let date = item.date.date;
 
         for(var game in item.games) {
+          item.games[game].parentKey = childSnap.key
           item.games[game].key = game
           item.games[game].date = date
           winnersBank.push(item.games[game])
@@ -199,9 +206,13 @@ export default {
       })
 
       this.winners = winnersBank.reverse();
-      this.activateDelete();
-//      this.getTopLeader()
+      this.activateEdit();
     })
+  },
+  watch: {
+    winners: function () {
+      this.getTopLeader()
+    }
   }
 }
 </script>
